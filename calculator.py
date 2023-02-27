46f752bd8afecd8df042d8c0c0f298a4cc32a31d
MarvinKilo <dangerhulk26022022@gmail.com>
2023-02-27 19:47:42 +0530

    Adding additional features modulus and power

--- calculator.py
+++ calculator.py
@@ -1,43 +1,25 @@
-# V-1.0
-# Done By Rushi, Mohan, Dhanunjay
-import sys
+# Function to add two numbers
+def add(x, y):
+    return x + y
 
-# Define the basic math operations
-def add(a, b):
-    return a + b
+# Function to subtract two numbers
+def subtract(x, y):
+    return x - y
 
-def subtract(a, b):
-    return a - b
+# Function to multiply two numbers
+def multiply(x, y):
+    return x * y
 
-def multiply(a, b):
-    return a * b
+# Function to divide two numbers
+def divide(x, y):
+    if y == 0:
+        raise ValueError("Cannot divide by zero")
+    return x / y
 
-def divide(a, b):
-    return a / b
+# Function to get the modulus of two numbers
+def modulus(x, y):
+    return x % y
 
-# Get user input for numbers and operation
-try:
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the math operation (+, -, *, /): ")
-except ValueError:
-    print("Invalid input!")
-    sys.exit()
-
-# Perform the operation based on user input
-if operation == "+":
-    print("Result: ", add(num1, num2))
-elif operation == "-":
-    print("Result: ", subtract(num1, num2))
-elif operation == "*":
-    print("Result: ", multiply(num1, num2))
-elif operation == "/":
-    if num2 == 0:
-        print("Error: Division by zero!")
-        sys.exit()
-    else:
-        print("Result: ", divide(num1, num2))
-else:
-    print("Invalid operation!")
-
-
+# Function to get the power of a number
+def power(x, y):
+    return x ** y
